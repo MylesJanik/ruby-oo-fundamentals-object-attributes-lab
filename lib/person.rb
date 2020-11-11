4fcbaa9c86a7fc34ec19825551f821914ca72d60
Myles <mylesjanik@gmail.com>
2020-11-11 15:26:01 -0800

    commit tree ee0700c911f0d16793437fe2739822f4bf58f216

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+    def name
+        return @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def job
+        return @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+end
